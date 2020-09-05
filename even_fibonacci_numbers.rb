d9b7b49d6dbd10c2b9a65514bddc478f40c5320b
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-09-05 23:54:59 +0100

    Add even_fibonacci_numbers solution

---
+++ even_fibonacci_numbers.rb
@@ -0,0 +1,19 @@
+
+def fibanoccai(num)
+  fibarr = []
+  i = 0
+  switch = true
+  while switch
+    if i.zero?
+      fibarr << i + 1
+    elsif i == 1
+      fibarr << i + fibarr[fibarr.length - 1]
+    elsif (fibarr[fibarr.length - 1] + fibarr[fibarr.length - 2]) < num
+      fibarr << fibarr[fibarr.length - 1] + fibarr[fibarr.length - 2]
+    else
+      switch = false
+    end
+    i += 1
+    end
+  fibarr.select(&:even?).inject(:+)
+end
